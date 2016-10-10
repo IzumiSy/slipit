fc7100c66d71f570fced8038d533b9943174b26b
IzumiSy <beetle-noise@gmx.com>
2016-10-10 21:53:42 +0900

    Remove :show action and add :search action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :bookmark
+  resources :bookmark, except: [:show] do
+    collection do
+      get 'search'
+    end
+  end
 
   root 'bookmark#index'
 
@@ -8,21 +12,6 @@ Rails.application.routes.draw do
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
 
   # Example resource route with sub-resources:
   #   resources :products do
