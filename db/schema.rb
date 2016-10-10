5b3fc10a18239bf817e81c51e0e613aa6faa08da
IzumiSy <beetle-noise@gmx.com>
2016-10-10 18:23:39 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010080737) do
+ActiveRecord::Schema.define(version: 20161010091647) do
 
   create_table "bookmarks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
