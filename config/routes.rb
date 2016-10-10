1fec4f7e6f3c554f31134ac878ce7b6e292aaa71
IzumiSy <beetle-noise@gmx.com>
2016-10-10 17:46:04 +0900

    Specify root

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   resources :bookmark
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'bookmark#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
