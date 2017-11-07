224c36f3e588c948f32e1a4b348d00779cd5db9d
IzumiSy <beetle-noise@gmx.com>
2017-11-07 14:48:55 +0900

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,10 +13,10 @@
 ActiveRecord::Schema.define(version: 20161020082155) do
 
   create_table "bookmarks", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "title"
-    t.string   "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "description"
   end
 
   create_table "tags", force: :cascade do |t|
