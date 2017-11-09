55e4e9b57fc7665d6f80110034beb80afc15083c
IzumiSy <beetle-noise@gmx.com>
2017-11-09 06:22:59 +0000

    Implement Api::BookmarksContller with create action

---
+++ app/controllers/api/bookmarks_controller.rb
@@ -0,0 +1,24 @@
+class Api::BookmarksController < ApplicationController
+  before_action :set_default_headers
+
+  def create
+    bookmark = Bookmark.new(bookmark_permitted_params)
+    if bookmark.save
+      render json: bookmark
+    else
+      render json: {
+        errors: bookmark.errors.full_messages
+      }
+    end
+  end
+
+  private
+
+  def api_bookmark_permitted_params
+    params.require(:bookmark).permit(:title, :url, :description)
+  end
+
+  def set_default_headers
+    response.headers['Content-Type'] = 'application/vnd.api-json'
+  end
+end
