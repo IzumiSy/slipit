6c3d06abb3d175c25c868bb3b935cd4286cadaf9
IzumiSy <beetle-noise@gmx.com>
2017-11-10 10:39:30 +0000

    Add a new layout and use it for clearnce

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,12 @@ module SlipIt
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Settings for Clearance
+    config.to_prepare do
+      Clearance::SessionsController.layout "login"
+      Clearance::UsersController.layout "login"
+      Clearance::PasswordsController.layout "login"
+    end
   end
 end
