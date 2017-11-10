71d2724b6bd3cab5e1219814b195b47e053bc2a6
IzumiSy <beetle-noise@gmx.com>
2017-11-11 00:54:54 +0900

    Simplify bookmarklet

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,29 +6,14 @@ class StaticPagesController < ApplicationController
   #
 
   def bookmarklet
-    @bookmarklet_js = <<-BOOKMARKLET
+    @src = <<-SRC
 javascript:(
   function() {
-    var request = new XMLHttpRequest();
-    request.open('POST', 'http://#{request.host}:#{request.port}#{api_bookmarks_path}');
-    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    request.onreadystatechange = function() {
-      if (request.readyState === 4) {
-        if (request.status === 200) {
-          alert("Bookmark added");
-        } else {
-          alert("Failed to add this page to bookmarks")
-        }
-      }
-    };
-    request.send(JSON.stringify({
-      "bookmark": {
-        "title": document.title,
-        "url": location.href
-      }
-    }));
+    window.open('http://#{request.host}:#{request.port}/#{new_bookmark_path}' +
+      '?title=' + encodeURI(document.title) + '&url=' + encodeURI(location.href));
   }
 )();
-    BOOKMARKLET
+      SRC
+    @bookmarklet = @src.delete("\n").delete(" ")
   end
 end
