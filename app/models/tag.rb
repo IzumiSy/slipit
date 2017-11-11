32ac56e7d90dae6ea34817ec8460cb56e6ebd07d
IzumiSy <beetle-noise@gmx.com>
2017-11-12 02:04:02 +0900

    Add two scopes

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,9 @@
 class Tag < ApplicationRecord
   has_and_belongs_to_many :bookmarks
   belongs_to :user
+
+  scope :order_by_name, -> { order(:name) }
+  scope :order_by_bookmark_counts, -> {
+    order("bookmark_counts DESC, name")
+  }
 end
