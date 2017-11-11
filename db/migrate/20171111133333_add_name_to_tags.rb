fe3ed5e81974508a96c2cf6c17dc584845373de6
IzumiSy <beetle-noise@gmx.com>
2017-11-11 22:35:22 +0900

    Update schema

--- db/migrate/20171111133333_add_name_to_tags.rb
+++ db/migrate/20171111133333_add_name_to_tags.rb
@@ -1,5 +1,5 @@
 class AddNameToTags < ActiveRecord::Migration[5.1]
   def change
-    add_column :tags, :name, :string
+    add_column :tags, :name, :string, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171111132353) do
+ActiveRecord::Schema.define(version: 20171111133333) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20171111132353) do
   create_table "tags", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
   end
 
   create_table "users", force: :cascade do |t|
