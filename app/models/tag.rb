4e0bb62a3490b9f02ba6d19a0acaa75a8f063905
IzumiSy <beetle-noise@gmx.com>
2017-11-12 00:11:05 +0900

    Now User has many Tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ApplicationRecord
   has_and_belongs_to_many :bookmarks
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   include Clearance::User
 
   has_many :bookmarks, dependent: :destroy
+  has_many :tags, dependent: :destroy
 end
