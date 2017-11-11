450ebb490720a8119a88ce309f891448edcb7d56
IzumiSy <beetle-noise@gmx.com>
2017-11-12 00:57:35 +0900

    Implement manipulation of counter cache of tags

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -6,4 +6,17 @@ class Bookmark < ApplicationRecord
     validates :title
     validates :url, url: true
   end
+
+  after_create :increase_tag_counters
+  after_destroy :decrease_tag_counters
+
+  private
+
+  def increase_tag_counters
+    self.tags.each { |tag| tag.bookmark_counts += 1 }
+  end
+
+  def decrease_tag_counters
+    self.tags.each { |tag| tag.bookmark_counts -= 1 }
+  end
 end
