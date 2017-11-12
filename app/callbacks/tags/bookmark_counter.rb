5f17e33f1194188b799893474bfe144a56678ee9
IzumiSy <beetle-noise@gmx.com>
2017-11-12 16:41:47 +0900

    Implement Tags::BookmarkCounter callback class

--- app/callbacks/tags/bookmark_counter.rb
+++ app/callbacks/tags/bookmark_counter.rb
@@ -1,13 +1,19 @@
 class Tags::BookmarkCounter
-  def after_create
-
+  def after_create(record)
+    perform_later(record.user.tags)
   end
 
-  def after_udpate
-
+  def after_update(record)
+    perform_later(record.user.tags)
   end
 
-  def after_destroy
+  def after_destroy(record)
+    perform_later(record.user.tags)
+  end
 
+  private
+
+  def perform_later(tags)
+    BookmarkCounterJob.perform_later(tags.pluck(:id))
   end
 end
