fe8dc8876b6be7c8068b7165d23626baa5b808bd
IzumiSy <beetle-noise@gmx.com>
2017-11-12 16:41:11 +0900

    Show SQL log executed by Resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,5 @@
 Resque.redis = Redis.new(url: ENV['REDIS_URL'])
-Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
+Resque.after_fork = Proc.new do
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
+  ActiveRecord::Base.establish_connection
+end
