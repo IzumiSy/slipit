80fd8286186e44095772d7a2f8ae8a8120a8b185
IzumiSy <beetle-noise@gmx.com>
2017-11-13 02:54:23 +0900

    Fix update and destroy logic of tags

--- app/jobs/bookmark_counter_job.rb
+++ app/jobs/bookmark_counter_job.rb
@@ -3,7 +3,9 @@ class BookmarkCounterJob < ApplicationJob
 
   def perform(tags)
     Tag.where(id: tags).each do |tag|
-      tag.update(bookmark_counts: tag.bookmark_counts)
+      bookmark_counts = tag.bookmarks.count
+      tag.update(bookmark_counts: bookmark_counts)
+      tag.destroy if bookmark_counts == 0
     end
   end
 end
