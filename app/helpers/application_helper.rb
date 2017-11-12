72537eb8251c788ed6dcd71ad2095e0b530053f3
IzumiSy <beetle-noise@gmx.com>
2017-11-12 20:49:00 +0900

    Add meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def meta_tag_configs
+    {
+      title: "Slip.it",
+      description: "Your online bookmarks never be social.",
+      keywords: "Bookmark, Social, Online",
+      og: {
+        title: "Slip.it",
+        type: "website",
+        url: "http://slipit.in",
+        image: asset_path("logo_small.png")
+      }
+    }
+  end
 end
