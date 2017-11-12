c665d45aaff83452cf0ba48eddf2cfadbce1dbb7
IzumiSy <beetle-noise@gmx.com>
2017-11-12 16:51:25 +0900

    Add validation of presence and uniqueness

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,11 @@ class Tag < ApplicationRecord
   has_and_belongs_to_many :bookmarks
   belongs_to :user
 
+  with_options presence: true do
+    validates :name, uniqueness: true
+    validates :bookmark_counts
+  end
+
   scope :order_by_name, -> { order(:name) }
   scope :order_by_bookmark_counts, -> {
     order("bookmark_counts DESC, name")
