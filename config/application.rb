78755a4db68f7a7819193647f600aaa3939e23fd
IzumiSy <beetle-noise@gmx.com>
2017-11-13 14:15:50 +0900

    Use Rack::Deflater

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module SlipIt
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # Middlewares
+    config.middleware.use Rack::Deflater
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
