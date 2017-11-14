e1b7b3ddc7b1d259f1c73536ee2c2745b41a803f
IzumiSy <beetle-noise@gmx.com>
2017-11-15 00:56:13 +0900

    Use only proc, not Proc.new

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,5 @@
 Resque.redis = Redis.new(url: ENV['REDIS_URL'])
-Resque.after_fork = Proc.new do
+Resque.after_fork = proc do
   ActiveRecord::Base.logger = Logger.new(STDOUT)
   ActiveRecord::Base.establish_connection
 end
