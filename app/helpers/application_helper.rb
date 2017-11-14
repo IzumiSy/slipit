581bf076aa26a6412bdfdcf56dd411b1ab6c4c13
IzumiSy <beetle-noise@gmx.com>
2017-11-15 01:27:42 +0900

    Auto correct for helpers/

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
   def metatag_configs
     {
-      title: "Slip.it",
-      description: "Your online bookmarks never be social.",
-      keywords: "Bookmark, Social, Online",
+      title: 'Slip.it',
+      description: 'Your online bookmarks never be social.',
+      keywords: 'Bookmark, Social, Online',
       og: {
-        title: "Slip.it",
-        type: "website",
-        url: "http://slipit.in",
-        image: image_url("logo_small.png")
+        title: 'Slip.it',
+        type: 'website',
+        url: 'http://slipit.in',
+        image: image_url('logo_small.png')
       }
     }
   end
