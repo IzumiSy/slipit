61edf9a1f2ec8cb0e80ee35619e5cca8484fc664
IzumiSy <beetle-noise@gmx.com>
2017-11-15 01:02:59 +0900

    Use nesting style

--- app/callbacks/tags/bookmark_counter.rb
+++ app/callbacks/tags/bookmark_counter.rb
@@ -1,19 +1,21 @@
-class Tags::BookmarkCounter
-  def after_create(record)
-    perform_later(record.user.tags)
-  end
+module Tags
+  class BookmarkCounter
+    def after_create(record)
+      perform_later(record.user.tags)
+    end
 
-  def after_update(record)
-    perform_later(record.user.tags)
-  end
+    def after_update(record)
+      perform_later(record.user.tags)
+    end
 
-  def after_destroy(record)
-    perform_later(record.user.tags)
-  end
+    def after_destroy(record)
+      perform_later(record.user.tags)
+    end
 
-  private
+    private
 
-  def perform_later(tags)
-    BookmarkCounterJob.perform_later(tags.pluck(:id))
+    def perform_later(tags)
+      BookmarkCounterJob.perform_later(tags.pluck(:id))
+    end
   end
 end
