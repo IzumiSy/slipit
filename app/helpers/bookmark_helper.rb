6df360456bcf76664cec651009db8b9bc69992ba
IzumiSy <beetle-noise@gmx.com>
2017-11-15 01:41:38 +0900

    Update title when searching

--- app/helpers/bookmark_helper.rb
+++ app/helpers/bookmark_helper.rb
@@ -2,4 +2,12 @@ module BookmarkHelper
   def favicon(url)
     "http://favicon.hatena.ne.jp/?url=#{url}"
   end
+
+  def search_title
+    if @bookmark_search.query
+      return "Search: \"#{@bookmark_search.query}\""
+    else
+      nil
+    end
+  end
 end
