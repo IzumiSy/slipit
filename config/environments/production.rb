8c748bb4ff70ff55010b00a7801e621ff5844e83
IzumiSy <beetle-noise@gmx.com>
2017-11-15 16:21:37 +0900

    Update production env to use sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,18 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "slip_it_#{Rails.env}"
+
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'slipit.me' }
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'slipit.me',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
