ab50693b8f54bea4e6221db584975d3715beaef1
IzumiSy <beetle-noise@gmx.com>
2017-11-16 08:08:20 +0900

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,19 +10,20 @@ Rails.application.routes.draw do
   get '/sign_up', to: 'users#new', as: :sign_up
   post '/sign_up', to: 'users#create', as: :users
 
-  scope module: :users do
-    resources :activations, only: %i[show], param: :token, path: :user_activates
+  namespace :users do
+    resource :activation, only: %i[show], param: :token, path: :activate
+    resource :password_reset, only: %i[create new], path: :password_reset
+  end
+
+  resources :users, only: %i[] do
+    scope module: :users do
+      resource :password, only: %i[create edit update]
+    end
   end
 
   scope module: :clearance do
-    resources :passwords, only: %i[create new]
-    resources :users, only: %i[] do
-      resource :password, controller: :passwords, only: %i[create edit update]
-    end
-
     get '/sign_in', to: 'sessions#new', as: :sign_in
     post '/sign_in', to: 'sessions#create', as: :session
-
     delete '/sign_out', to: 'sessions#destroy', as: :sign_out
   end
 
