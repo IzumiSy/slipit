9f0a3756723fde686491765bdc562d00eb8a2ad8
IzumiSy <beetle-noise@gmx.com>
2017-11-16 00:41:31 +0900

    Implement show action in Users::ActivationsController

--- app/controllers/users/activations_controller.rb
+++ app/controllers/users/activations_controller.rb
@@ -1,5 +1,17 @@
 class Users::ActivationsController < ApplicationController
   def show
-    # TODO
+    user = User.find_by(activation_token: params.require(:token))
+
+    unless user.activated_at
+      user.activated_at = Time.now
+      user.save!
+      sign_in user
+      redirect_to root_path, notice: 'Your account has successfull activated!'
+      return
+    end
+
+    redirect_to sign_in_path, notice: 'Your account has already activated'
+  rescue => _e
+    redirect_to sign_up_path
   end
 end
