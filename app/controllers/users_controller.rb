b0e2bba1c8626f8be42f0b67ccd549a878994eb6
IzumiSy <beetle-noise@gmx.com>
2017-11-15 23:47:53 +0900

    Fix wording

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < Clearance::UsersController
   def url_after_create
-    flash[:notice] = 'User confirmatil mail has sent to your email'
+    flash[:notice] = 'User confirmation mail has sent to your email'
     sign_in_path
   end
 end
