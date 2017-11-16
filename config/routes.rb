f53d07c90796f58d7a6f75ddf03cb1c78ec41143
IzumiSy <beetle-noise@gmx.com>
2017-11-16 09:11:28 +0900

    Remove unnecessary create route

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[show edit update] do
     scope module: :users do
-      resource :password_reset, only: %i[create edit update]
+      resource :password_reset, only: %i[edit update]
     end
   end
 
