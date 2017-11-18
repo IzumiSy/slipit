6dfc425e99543688ab86cbf6d99ab5907c291e4c
IzumiSy <beetle-noise@gmx.com>
2017-11-18 16:28:41 +0900

    Fix type of a column

--- db/migrate/20171118071637_add_tag_counts_to_user.rb
+++ db/migrate/20171118071637_add_tag_counts_to_user.rb
@@ -1,5 +1,5 @@
 class AddTagCountsToUser < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :tag_coutns, :integer, null: false, default: 0
+    add_column :users, :tag_counts, :integer, null: false, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,15 +40,15 @@ ActiveRecord::Schema.define(version: 20171118071637) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "encrypted_password", limit: 128
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128
-    t.string "activation_token"
     t.integer "bookmark_counts", default: 0, null: false
-    t.integer "tag_coutns", default: 0, null: false
     t.datetime "activated_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "activation_token"
+    t.integer "tag_counts", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["remember_token"], name: "index_users_on_remember_token", unique: true
   end
