1bde7595e6dc3a252bfa5520702ba1ab5cfd9d9a
IzumiSy <beetle-noise@gmx.com>
2017-11-18 16:04:30 +0900

    Implement user info update

--- app/controllers/users/me_controller.rb
+++ app/controllers/users/me_controller.rb
@@ -4,12 +4,19 @@ class Users::MeController < ApplicationController
   before_action :require_login
 
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    if current_user.update(update_user_permitted_params)
+      redirect_to users_me_path, notice: "Your account has successfully updated"
+    else
+      redirect_to users_me_path, alert: "Failed to update your account"
+    end
   end
 
   def destroy
@@ -17,5 +24,11 @@ class Users::MeController < ApplicationController
     sign_out
     user.delete
     redirect_to sign_in_path, notice: 'Your account has successfully deleted'
+  end
+
+  private
+
+  def update_user_permitted_params
+    params.require(:user).permit(:email)
   end
 end
