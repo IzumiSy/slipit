b7a4efde93f208d4989ce54789b6381fb75056ba
IzumiSy <beetle-noise@gmx.com>
2017-12-14 16:53:40 +0900

    Remove preload

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,7 +6,7 @@ class BookmarksController < ApplicationController
 
   def index
     @new_bookmark = NewBookmarkForm.new
-    @bookmarks = @bookmark_search.call.order_by_created_at.preload(:tags)
+    @bookmarks = @bookmark_search.call.order_by_created_at
     @user = current_user
   end
 
