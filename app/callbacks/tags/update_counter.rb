671062f37f65c111075401ab438583cf13484f69
IzumiSy <beetle-noise@gmx.com>
2017-12-14 15:54:55 +0900

    Remove Users::TagCounterJob and its kick-in callback

--- app/callbacks/tags/update_counter.rb
+++ app/callbacks/tags/update_counter.rb
@@ -1,15 +1,9 @@
 class Tags::UpdateCounter
   def after_create(tag)
-    update_user_tag_counts(tag)
+    # update_user_tag_counts(tag)
   end
 
   def after_destroy(tag)
-    update_user_tag_counts(tag)
-  end
-
-  private
-
-  def update_user_tag_counts(tag)
-    Users::TagCounterJob.perform_later(tag.user_id)
+    # update_user_tag_counts(tag)
   end
 end
--- app/jobs/users/tag_counter_job.rb
+++
@@ -1,8 +0,0 @@
-class Users::TagCounterJob < ApplicationJob
-  queue_as :default
-
-  def perform(user_id)
-    user = User.find(user_id)
-    user.update(tag_counts: user.tags.count)
-  end
-end
