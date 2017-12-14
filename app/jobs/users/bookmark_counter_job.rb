d24851001d20d99b6c23cec52df0e6da04dfc76c
IzumiSy <beetle-noise@gmx.com>
2017-12-14 15:40:28 +0900

    Remove bookmark count from jobs

--- app/jobs/users/bookmark_counter_job.rb
+++
@@ -1,8 +0,0 @@
-class Users::BookmarkCounterJob < ApplicationJob
-  queue_as :default
-
-  def perform(user_id)
-    user = User.find(user_id)
-    user.update(bookmark_counts: user.bookmarks.count)
-  end
-end
