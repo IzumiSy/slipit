1271e756046852707a9209e2f53e734742e34aa3
IzumiSy <beetle-noise@gmx.com>
2017-12-18 19:06:37 +0900

    Update og description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,7 @@ module ApplicationHelper
     {
       site: 'Slip.it',
       title: 'Your online bookmarks never be social.',
-      description: 'Sick and tired of the social network services out there?
-        We get you covered! Slip.it is the 100% private, simple online bookmarking service.',
+      description: 'Slip.it is the 100% private, simple online bookmarking service.',
       keywords: 'Bookmark, Social, Online',
       reverse: true,
       og: {
