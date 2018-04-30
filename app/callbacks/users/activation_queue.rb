314c01ae641f7b91af8c9df4c46d244bf7b72225
IzumiSy <beetle-noise@gmx.com>
2018-04-30 21:42:42 +0900

    Fix a bug: failure of retrieving user id

--- app/callbacks/users/activation_queue.rb
+++ app/callbacks/users/activation_queue.rb
@@ -1,6 +1,6 @@
 class Users::ActivationQueue
   def after_create(user)
-    user = User.find(user_id)
+    user = User.find(user.id)
     user.activation_token = Clearance::Token.new
     user.save
     UsersMailer.activation(user).deliver_now
