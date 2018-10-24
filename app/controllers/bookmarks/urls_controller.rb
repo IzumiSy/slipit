1b2ab897d21097481c21fd2e7d68dd1b14a8ffc9
IzumiSy <beetle-noise@gmx.com>
2018-10-24 16:24:13 +0900

    Fix #68

--- app/controllers/bookmarks/urls_controller.rb
+++ app/controllers/bookmarks/urls_controller.rb
@@ -14,7 +14,8 @@ class Bookmarks::UrlsController < ApplicationController
 
     result = new_bookmark.call
     if result.failed?
-      redirect_to bookmarks_path, alert: result.reason && return
+      redirect_to bookmarks_path, alert: result.reason
+      return
     end
 
     redirect_to new_bookmark_url(new_bookmark.to_h)
