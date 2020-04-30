c8d44216acaa501e99ef8d6d7f1c9080fe4419db
IzumiSy <beetle-noise@gmx.com>
2020-04-30 17:20:04 +0900

    Update timing to take screenshots

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -12,36 +12,39 @@ RSpec.feature "User", type: :feature do
 
   scenario 'can get logged in if activated' do
     visit root_path
-    take_screenshot
     click_on 'Log in'
     expect(page).to have_content 'Login to Slip.it'
 
     fill_in 'session_email', with: 'activated_user@example.com'
     fill_in 'session_password', with: 'password'
+    take_screenshot
+
     click_on 'Login'
     expect(page).to have_content 'Add a new bookmark'
   end
 
   scenario 'can not get logged in if not activated' do
     visit root_path
-    take_screenshot
     click_on 'Log in'
     expect(page).to have_content 'Login to Slip.it'
 
     fill_in 'session_email', with: 'unactivated_user@example.com'
     fill_in 'session_password', with: 'password'
+    take_screenshot
+
     click_on 'Login'
     expect(page).to have_content 'You must confirm your email address'
   end
 
   scenario 'can not get logged in if not registered' do
     visit root_path
-    take_screenshot
     click_on 'Log in'
     expect(page).to have_content 'Login to Slip.it'
 
     fill_in 'session_email', with: 'unregistered_user@example.com'
     fill_in 'session_password', with: 'password'
+    take_screenshot
+
     click_on 'Login'
     expect(page).to have_content 'Bad email or password'
   end
